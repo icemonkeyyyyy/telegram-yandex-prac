4ce85a1a0c72adf35d0326d70b9ab727619cf2c2
icemonkeyyyyy <tikhon_karpov@mail.ru>
2023-12-10 17:40:26 +0300

    Add message filter

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from config import API_TELEGRAM_TOKEN, chat_id
 bot = telebot.TeleBot(token=API_TELEGRAM_TOKEN)
 
 
+def filter_text(message):
+    text = "привет"
+    return text in message.text
+
+
 @bot.message_handler(content_types=['text'])
 def repeat_message(message):  # Функция для обработки сообщений
     bot.send_message(message.chat.id, message.text)  # Отправка ответа
