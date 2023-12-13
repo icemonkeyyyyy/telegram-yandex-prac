ccf7ea23a202d158c05f02145b49dc3989360890
icemonkeyyyyy <tikhon_karpov@mail.ru>
2023-12-13 19:25:39 +0300

    Filter

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ bot = telebot.TeleBot(token=API_TELEGRAM_TOKEN)
 
 
 def filter_text(message):
-    text = "привет"
-    return text in message.text
+    text = ["привет"]
+    return message.text.lower() in text
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=['text'], func=filter_text)
 def repeat_message(message):  # Функция для обработки сообщений
     bot.send_message(message.chat.id, message.text)  # Отправка ответа
 
